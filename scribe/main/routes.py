5d92158dacf65f9cd23a25cbe6dd9e2818267e7d
Egbe Eugene <agboreugene@gmail.com>
2019-11-26 23:03:03 +0100

    Add route to get section data in service
    - Added getSectionData route get data to client
    - Added get_section_data utility function to perform database request
    
    Bug: [T239285](https://phabricator.wikimedia.org/T239285)

--- scribe/main/routes.py
+++ scribe/main/routes.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from flask import Blueprint, request
-from scribe.main.utils import get_reference_resource_data
+from scribe.main.utils import get_reference_resource_data, get_section_data
 
 
 main = Blueprint('main', __name__)
@@ -12,6 +12,16 @@ main = Blueprint('main', __name__)
 def getReferencesData():
     article_name = request.args.get('article')
     resource_data = get_reference_resource_data(article_name)
+    if resource_data:
+        return resource_data
+    else:
+        return '<h2> Welcome to Scribe service</h2>'
+
+
+@main.route('/api/v1/sections')
+def getSectionData():
+    article_name = request.args.get('article')
+    resource_data = get_section_data(article_name)
     if resource_data:
         return resource_data
     else:
--- scribe/main/utils.py
+++ scribe/main/utils.py
@@ -1,7 +1,7 @@
 
 import sys
 
-from scribe.models import Edit
+from scribe.models import Edit, Section
 
 
 def get_reference_resource_data(article_name):
@@ -13,7 +13,7 @@ def get_reference_resource_data(article_name):
     """
     resource_object = {}
     resource_object['resources'] = []
-    edit_data = Edit.query.filter_by(article_name=article_name).all()
+    edit_data = Edit.query.filter_by(article_name=article_name.lower()).all()
     for data in edit_data:
         data_object = {}
         data_object['section_label'] = data.section_label
@@ -23,3 +23,21 @@ def get_reference_resource_data(article_name):
         resource_object['resources'].append(data_object)
     resource_object['article_name'] = article_name
     return resource_object
+
+
+def get_section_data(article_name):
+    section_count = 0
+    all_sections_data = {}
+    parse = {}
+    sections = []
+    sections_data = Section.query.filter_by(article_name=article_name.lower()   ).all()
+    print(sections_data, file=sys.stderr)
+    for section_data in sections_data:
+        section = {}
+        section['line'] = section_data.label
+        section['number'] = str(section_count + 1)
+        sections.append(section)
+        section_count += 1
+    parse['sections'] = sections
+    all_sections_data['parse'] = parse
+    return all_sections_data
